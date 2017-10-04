0e3a7ff2cdfcc0cb455a17d879c4e12f12f2d809
WATCHDOGS <taned@intbizth.com>
2017-10-04 16:47:00 +0700

    init API test for intern

---
+++ app/AppKernel.php
@@ -0,0 +1,90 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = [
+            // Resources -- First!
+            new \Sylius\Bundle\MailerBundle\SyliusMailerBundle(),
+            new \Sylius\Bundle\UserBundle\SyliusUserBundle(),
+            new \PhpMob\CoreBundle\PhpMobCoreBundle(),
+            new \Toro\CoinBundle\ToroCoinBundle(),
+            new \Toro\RegisterBundle\ToroRegisterBundle(),
+
+            // CMS
+            new \PhpMob\CmsBundle\PhpMobCmsBundle(),
+            new \Sylius\Bundle\LocaleBundle\SyliusLocaleBundle(),
+            new \Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+
+            // Media
+            new \PhpMob\MediaBundle\PhpMobMediaBundle(),
+            new \Oneup\FlysystemBundle\OneupFlysystemBundle(),
+            new \Liip\ImagineBundle\LiipImagineBundle(),
+            //new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
+            new \FM\ElfinderBundle\FMElfinderBundle(),
+
+            // SF3
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // Core
+            new \Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
+            new \Sylius\Bundle\GridBundle\SyliusGridBundle(),
+            new \winzou\Bundle\StateMachineBundle\winzouStateMachineBundle(),
+            new \PhpMob\ChangMinBundle\ChangMinBundle(),
+            new \Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new \Sylius\Bundle\FixturesBundle\SyliusFixturesBundle(),
+            new \Sonata\IntlBundle\SonataIntlBundle(),
+            new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new \PhpMob\TwigModifyBundle\PhpMobTwigModifyBundle(),
+        ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+
+            if ('dev' === $this->getEnvironment()) {
+                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+            }
+        }
+
+        return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
